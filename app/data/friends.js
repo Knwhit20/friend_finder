5efc31c9a9a70142f97905a61691aaddce7469b7
Kristen Stroup <kristenwhitson@gmail.com>
2019-10-13 12:11:33 -0500

    fix node_modules, unable to run server

--- app/data/friends.js
+++ app/data/friends.js
@@ -112,3 +112,4 @@ var friendsData = [
         ]
     },
 }
+module.exports = friendsData;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require("express");
+var path = require("path");
 // Tells node that we are creating an "express" server
 var app = express();
 
-var port = process.env.port || 8080
+var port = process.env.PORT || 8080;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//routes 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+// routes 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
 })
+
