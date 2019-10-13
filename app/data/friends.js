18f30e25a3c72469baa9bf87893a9802abede618
Kristen Stroup <kristenwhitson@gmail.com>
2019-10-13 15:34:14 -0500

    friends data array updated

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friendsData = [
     {
-        "name": "Ahmed",
+        "name": "Kayne",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
             "scores": [
                 5,
-                1,
-                4,
-                4,
                 5,
-                1,
-                2,
                 5,
-                4,
-                1
+                5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                5
             ]
     },
     {
-        "name": "Ahmed",
+        "name": "Courtney",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
             5,
@@ -32,7 +32,23 @@ var friendsData = [
         ]
     },
     {
-        "name": "Ahmed",
+        "name": "Cameron",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            2,
+            2,
+            4,
+            4,
+            1,
+            1,
+            5,
+            3,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Chris",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
             5,
@@ -48,67 +64,51 @@ var friendsData = [
         ]
     },
     {
-        "name": "Ahmed",
+        "name": "Val",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            5,
+            2,
+            3,
             1,
-            4,
-            4,
-            5,
+            1,
             1,
             2,
+            2,
+            1,
             5,
+            4
+        ]
+    },
+    {
+        "name": "Kyle",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
             4,
+            4,
+            3,
+            3,
+            3,
+            2,
+            2,
+            1,
+            1,
             1
         ]
     },
     {
-        "name": "Ahmed",
+        "name": "Jimmy",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            5,
             1,
-            4,
-            4,
-            5,
             1,
             2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
-            5,
-            4,
-            1
-        ]
-    },
-    {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
             2,
-            5,
+            3,
+            3,
+            3,
             4,
-            1
+            4
         ]
     },
 }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 var express = require("express");
 var path = require("path");
 // Tells node that we are creating an "express" server
 var app = express();
 
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,6 +13,8 @@ app.use(express.json());
 // routes 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
+
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
